1c3c7b625fd84b52ec1290b10ecf7dd70f3624a0
최지욱 <86818323+choijiuk1017@users.noreply.github.com>
2026-07-28 21:35:05 +0900

    기본 스크립트 구조 생성

---
+++ Source/Project_Gwido/Character/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Unit.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_GWIDO_API APlayerCharacter : public AUnit
+{
+	GENERATED_BODY()
+	
+};
--- Source/Project_Gwido/Project_Gwido.Build.cs
+++ Source/Project_Gwido/Project_Gwido.Build.cs
@@ -12,6 +12,8 @@ public class Project_Gwido : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PublicIncludePaths.Add("Project_Gwido");
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
